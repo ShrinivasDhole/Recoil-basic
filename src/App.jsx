93190637d91a520d201b59ca0113bb42c3d321bd
ShrinivasDhole <shrinivasdhole7@gmail.com>
2024-02-09 12:47:16 +0530

    Recoil file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,50 @@
-import { useContext, useState } from 'react'
 
 import './App.css'
-import { CountContext } from './context';
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil';
+import { countAtom, evenSelector } from './store/atoms/count';
 
 function App() {
-  const [count , setCount] = useState(0);
   //wrap anyone that wants to use the teleported value inside a provider
   return (
     <div>
-      <CountContext.Provider value={[count,setCount]}>
-
+      <RecoilRoot>
         <Count ></Count>
-      </CountContext.Provider>
+      </RecoilRoot>
     </div>
   )
 }
 
 function Count(){
-  const setCount = useContext(CountContext);
   return <div>
     <CounterRerender></CounterRerender>
-    <Buttons setCount={setCount}></Buttons>
+    <Buttons></Buttons>
   </div>
 }
 
 function CounterRerender(){
-  const count = useContext(CountContext);
+  const count = useRecoilValue(countAtom);
   return <div>
-    {count}
+    {count} <br />
+    <EvenSelector></EvenSelector>
+    </div>
+}
+
+function EvenSelector(){
+  const isEven = useRecoilValue(evenSelector);
+  return <div>
+    {isEven ? "Count is even" : null }
   </div>
 }
 
 function Buttons(){
-  const {count ,setCount}  = useContext(CountContext)
+  const setCount = useSetRecoilState(countAtom);
+  console.log("Button re-renders")
   return <div>
     <button onClick={()=>{
-      setCount(count+1)
+      setCount(count => count + 1)
     }}>Increase Count</button>
     <button onClick={()=>{
-      setCount(count-1)
+      setCount(count => count - 1)
     }}>Descrease Count</button>
   </div>
 }
